0febb946dd37ee441b2ffe28463a4fe015f38757
hamerkillers <hamerkillers@gmail.com>
2026-01-02 20:54:46 +0800

    publish:v0.1.13

--- src/helper/core/page.rs
+++ src/helper/core/page.rs
@@ -13,3 +13,11 @@ pub struct PageResponse<T> {
     pub current: i64,
     pub size: i64,
 }
+
+pub fn to_offset_limit(current: usize, size: usize) -> (i64, i64) {
+    let current = if current == 0 { 1 } else { current };
+    let size = if size == 0 { 10 } else { size };
+    let offset = ((current - 1) * size) as i64;
+    let limit = size as i64;
+    (offset, limit)
+}
